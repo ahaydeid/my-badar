9861e189fe30cc92dd00b592ccac4d2dd73781f6
ahaydeid <adi.hadi270@gmail.com>
2025-11-07 21:11:23 +0700

    Update

--- app/guru/attendance/comps/AttendanceHeader.tsx
+++ app/guru/attendance/comps/AttendanceHeader.tsx
@@ -3,7 +3,7 @@ import DateDisplay from "@/app/components/DateDisplay";
 export default function AttendanceHeader({ total, filled, kelasName }: { total: number; filled: number; kelasName?: string | null }) {
   return (
     <>
-      <p className="text-xs sm:text-sm md:text-sm font italic text-gray-500 mb-2 text-center flex-1">
+      <p className="text-xs sm:text-sm md:text-sm font italic text-gray-500 mt-2 text-center flex-1">
         <DateDisplay />
       </p>
       <div className="bg-white p-3 shadow-sm relative overflow-hidden">
@@ -11,7 +11,7 @@ export default function AttendanceHeader({ total, filled, kelasName }: { total: 
           <span>
             {filled}/{total}
           </span>
-          <p className="text-lg font-bold mb-2">{kelasName}</p>
+          <p className="text-lg font-bold">{kelasName}</p>
           <span className="text-gray-400"> ... </span>
         </div>
       </div>
